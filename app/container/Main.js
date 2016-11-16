9126e4dd817235b909f9bda18d33c3c1a67f82ba
Djole <sinisadjordjic@gmail.com>
2016-11-16 23:10:18 +0100

    Uradjen drugi korak, rutiranje

---
+++ app/container/Main.js
@@ -0,0 +1,14 @@
+var React = require('react');
+var PropTypes = React.PropTypes;
+
+var HelloWorld = React.createClass({
+
+  render: function() {
+    return (
+      <div>Hello World</div>
+    );
+  }
+
+});
+
+module.exports = HelloWorld;
