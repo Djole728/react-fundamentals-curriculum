435f38c47ece2ca05df3742e096006d0ed48d215
Djole <sinisadjordjic@gmail.com>
2016-11-20 21:01:45 +0100

    Finish step 4

--- app/container/MainContainer.js
+++ app/container/MainContainer.js
@@ -1,21 +1,33 @@
 var React = require('react');
 var PropTypes = React.PropTypes;
+var GetCityContainer = require('./GetCityContainer');
 
 var styles = {
-    container: {
-        width: '100%',
-        height: '92%'
-    }
+  container: {
+    width: '100%',
+    height: '92%'
+  },
+  header :{
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    background: 'rgba(252, 90, 44, 0.89)',
+    color: '#fff',
+    padding: 5,
+  }
 }
 
 var MainContainer = React.createClass({
 
     render: function() {
         return (
-            <div style={styles.container}>
-              <h1>Main.js Header</h1>
-                {this.props.children}
+          <div style={styles.container}>
+            <div style={styles.header}>
+              <h2 style={{margin: 0}}>Clever Title</h2>
+              <GetCityContainer direction='row' />
             </div>
+            {this.props.children}
+          </div>
         );
     }
 
